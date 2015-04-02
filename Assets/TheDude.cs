27ecf1cb967f7ed9c850e296cca93aace25feb8d
jtronlabs <jtronlabs@gmail.com>
2015-04-01 21:58:14 -0400

    initial commit

---
+++ Assets/TheDude.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TheDude : MonoBehaviour {
+
+	Animator animateTheDude;
+	// Use this for initialization
+	void Start () {
+		animateTheDude = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float vertical = Input.GetAxis ("Vertical");
+		float horizontal = Input.GetAxis ("Horizontal");
+		animateTheDude.SetFloat("speed", vertical );
+		animateTheDude.SetFloat ("Direction", horizontal, 0.25f, Time.deltaTime);
+		if( Input.GetKeyDown(KeyCode.Space) ){
+			animateTheDude.SetTrigger ("jump");
+		}
+		if( Input.GetKeyDown(KeyCode.Alpha1) ){
+			animateTheDude.SetTrigger ("die");
+		}
+		if( Input.GetKeyDown(KeyCode.Alpha2) ){
+			animateTheDude.SetTrigger ("revive");
+		}
+		if( Input.GetKeyDown(KeyCode.Alpha3) ){
+			animateTheDude.SetTrigger ("wave");
+		}
+	}
+}

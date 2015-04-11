9132bb1f7c2f014718f2fecfa7d9e2046b32293f
jtronlabs <jtronlabs@gmail.com>
2015-04-11 00:45:45 -0400

    removed some un-needed components, added textures to walls, added a door, a cube blocking it, and some more walls, added flip gravity with G

--- Assets/TheDude.cs
+++ Assets/TheDude.cs
@@ -4,14 +4,16 @@ using System.Collections;
 public class TheDude : MonoBehaviour {
 
 	Animator animateTheDude;
+	bool isFlipping,isOnRoof;
 	// Use this for initialization
 	void Start () {
 		animateTheDude = GetComponent<Animator> ();
+		isFlipping = false;
+		isOnRoof = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//this.rigidbody.MovePosition (transform.position + transform.forward * Time.deltaTime);
 		float vertical = Input.GetAxis ("Vertical");
 		float horizontal = Input.GetAxis ("Horizontal");
 		animateTheDude.SetFloat("speed", vertical );
@@ -28,5 +30,23 @@ public class TheDude : MonoBehaviour {
 		if( Input.GetKeyDown(KeyCode.Alpha3) ){
 			animateTheDude.SetTrigger ("wave");
 		}
+		if(Input.GetKeyDown(KeyCode.G))
+		{
+			isFlipping = true;
+			isOnRoof = !isOnRoof;
+			rotateCharacter();
+			Physics.gravity *= -1;
+		}
+
+		//if character is touching ground then isFlipping = false;
+	}
+	
+	//need a function to smoothly rotate character. Should probably change speed of rotation depending on distance from walls.
+	//Must ensure that character has flipped by the time he touches next wall
+	//this might be useful http://codereview.stackexchange.com/questions/68217/rotating-a-character-upside-down-and-vice-versa
+	private void rotateCharacter(){ 
+		Vector3 rot = this.rigidbody.rotation.eulerAngles; 
+		rot = new Vector3(rot.x,rot.y+180,rot.z);
+		this.rigidbody.rotation = Quaternion.Euler(rot);
 	}
 }

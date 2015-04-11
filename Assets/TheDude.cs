985500ddda517ea18ee83de8df653c9d40d8988c
jtronlabs <jtronlabs@gmail.com>
2015-04-11 01:30:26 -0400

    simple rotation while gravity inverts. It seems that gravity doesn't work while person is rotating (though I may be wrong on this). Also not sure how to prevent starting rotation while person is in the air.

--- Assets/TheDude.cs
+++ Assets/TheDude.cs
@@ -4,16 +4,18 @@ using System.Collections;
 public class TheDude : MonoBehaviour {
 
 	Animator animateTheDude;
-	bool isFlipping,isOnRoof;
+	bool characterRotating;
+	float rotationRate = 2f;
+
 	// Use this for initialization
 	void Start () {
 		animateTheDude = GetComponent<Animator> ();
-		isFlipping = false;
-		isOnRoof = false;
+		characterRotating = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//Animation controllers
 		float vertical = Input.GetAxis ("Vertical");
 		float horizontal = Input.GetAxis ("Horizontal");
 		animateTheDude.SetFloat("speed", vertical );
@@ -30,14 +32,16 @@ public class TheDude : MonoBehaviour {
 		if( Input.GetKeyDown(KeyCode.Alpha3) ){
 			animateTheDude.SetTrigger ("wave");
 		}
-		if(Input.GetKeyDown(KeyCode.G))
+
+		//Gravity controller
+		if( !characterRotating && Input.GetKeyDown(KeyCode.G) ) //cannot rotate while in air - how to check that?
 		{
-			isFlipping = true;
-			isOnRoof = !isOnRoof;
-			rotateCharacter();
+			//set speed of rotation based on how far away wall is (if possible)
+			characterRotating = true;
 			Physics.gravity *= -1;
 		}
 
+		rotate ();
 		//if character is touching ground then isFlipping = false;
 	}
 	
@@ -45,8 +49,45 @@ public class TheDude : MonoBehaviour {
 	//Must ensure that character has flipped by the time he touches next wall
 	//this might be useful http://codereview.stackexchange.com/questions/68217/rotating-a-character-upside-down-and-vice-versa
 	private void rotateCharacter(){ 
-		Vector3 rot = this.rigidbody.rotation.eulerAngles; 
-		rot = new Vector3(rot.x,rot.y+180,rot.z);
-		this.rigidbody.rotation = Quaternion.Euler(rot);
+		//var distance = Vector3.Distance(object1.transform.position, object2.transform.position);//calc distance between 2 objects
+		Vector3 rot = this.transform.rotation.eulerAngles; 
+		rot = new Vector3(rot.x,rot.y,rot.z+180);
+		this.transform.rotation = Quaternion.Euler(rot);
+	}
+	/*
+	private void rotate(){
+		float totalRotation = 0;
+		while (characterRotating) {
+			if(totalRotation >= 180){
+				characterRotating = false;
+			}else{
+				Vector3 rot = this.transform.rotation.eulerAngles;
+				transform.Rotate(0, 0, rotationRate * Time.deltaTime);
+				totalRotation += rotationRate * Time.deltaTime;
+			}
+		}
+
+	}*/
+
+	private void rotate(){
+		if (characterRotating) {
+			transform.Rotate(0,0,rotationRate);
+			
+			Vector3 rot = this.transform.rotation.eulerAngles; 
+			//check if character is done rotating
+			if(Mathf.Abs(rot.z)<1){//rightside up
+				//smooth rotation
+				rot = new Vector3(rot.x,rot.y,0);
+				this.transform.rotation = Quaternion.Euler(rot);
+				//signal rotation has ended
+				characterRotating = false;
+			}else if( Mathf.Abs(rot.z-180)<1){//upside down
+				//smooth rotation
+				rot = new Vector3(rot.x,rot.y,180);
+				this.transform.rotation = Quaternion.Euler(rot);
+				//signal rotation has ended
+				characterRotating = false;
+			}
+		}
 	}
 }

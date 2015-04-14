88c65f1c29c6ff2df7ade381fa1386ca9c9c8e45
Taumer Anabtawi <tanabtawi147@gmail.com>
2015-04-14 10:53:02 -0400

    Added startup scene and created menus to access different levels. To build: Must add all scenes to build settings (file -> build settings)

---
+++ Assets/LoadOnClick.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour {
+	
+	public GameObject loadingImage;
+	
+	public void LoadScene(string name)
+	{
+		//loadingImage.SetActive(true);
+		Application.LoadLevel(name);
+	}
+}

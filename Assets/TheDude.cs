d5af918e299b06d3132a807700506526fe41aa56
tamd2 <tanabtawi147@gmail.com>
2015-04-15 19:18:38 -0400

    Added background music to all levels, put code for pausing/collision/gui in level 1 code

--- Assets/TheDude.cs
+++ Assets/TheDude.cs
@@ -9,6 +9,12 @@ public class TheDude : MonoBehaviour {
 	float roofHeight;
 	float groundHeight;
 	bool currentlyFlipped = false; //based on original oritenation flipped is flipped from beginning
+	bool isPaused;
+	Rect pauseMenu;
+	AudioSource boxSource;
+	GameObject box;
+	GameObject theGuy;
+	
 	
 	// Use this for initialization
 	void Start () {
@@ -16,6 +22,14 @@ public class TheDude : MonoBehaviour {
 		characterRotating = false;
 		roofHeight = GameObject.Find ("Roof").transform.position.y; //roof height
 		groundHeight = 0; 
+		//pauseCanvas = GameObject.Find ("PauseCanvas");
+		//pauseCanvas.SetActive (false);
+		isPaused = false;
+		pauseMenu = new Rect (0, 0, Screen.width, Screen.height);
+		box = GameObject.Find ("CubeBlockingDoor");
+		boxSource = box.GetComponent<AudioSource> ();
+		theGuy = GameObject.Find ("Dude");
+		
 	}
 	
 	// Update is called once per frame
@@ -37,6 +51,14 @@ public class TheDude : MonoBehaviour {
 		if( Input.GetKeyDown(KeyCode.Alpha3) ){
 			animateTheDude.SetTrigger ("wave");
 		}
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			isPaused = !isPaused; //flip now, first hit makes this true, is it now paused? is what this does
+			if(isPaused)
+				pause (); //run pause
+			else
+				unPause(); //handles multiple cases and inuts
+		}
+
 
 
 		//Gravity controller
@@ -132,6 +154,89 @@ public class TheDude : MonoBehaviour {
 		Vector3 respawnPos = respawnPoint.transform.position;
 		this.transform.position = respawnPos;
 		Debug.Log (respawnPos);
+	}
+
+	
+	//enter this if game is to be paused now
+	//seprated to allow for seprate music etc.
+	private void pause()
+	{
+		//needs to be paused
+		Time.timeScale = 0;
+		//gets checked every frame, so as long as it is paused this GUI menu gets polled WIHTOUT explicilty calling ONGUI
+	}
+	
+	//enter this if game is to be unPaused via hitting escape
+	private void unPause()
+	{
+		//just unpause
+		Time.timeScale = 1;
+	}
+	
+	public void newLoad(string name)
+	{
+		//these things happen regardless of what you are loading
+		Time.timeScale = 1; //re allow game back in
+		isPaused = false; //not paused
+		//pauseCanvas.SetActive (false);
+		if (currentlyFlipped) {
+			Physics.gravity *= - 1;
+			currentlyFlipped = false;
+		}
+		Application.LoadLevel (name);
+	}
+	
+	public void OnGUI(){
+		if (isPaused) {
+			GUI.Box (pauseMenu, "GAME PAUSED");
+			
+			// Make the Quit button.
+			//params: LEFT TOP WIDTH HEIGHT
+			if (GUI.Button (new Rect (Screen.width/2-50, Screen.height/2-100, 100, 50), "Resume")) {
+				isPaused = false; //flips
+				Time.timeScale = 1;
+				//GetComponent (MouseLook).enabled = true; //resumes game, as isPaused is false
+			}
+			if (GUI.Button (new Rect (Screen.width/2-50, Screen.height/2-50, 100, 50), "Main Menu"))
+			{
+				newLoad ("Startup");
+			}
+			if (GUI.Button (new Rect (Screen.width/2-50, Screen.height/2, 100, 50), "Retry"))
+			{
+				newLoad (Application.loadedLevelName);
+			}
+			if (GUI.Button (new Rect (Screen.width/2-50, Screen.height/2+50, 100, 50), "Quit")) {
+				Application.Quit(); //IN EDITOR, THERE IS NO FUNCTION OF THIS
+			}
+		}
+	}
+	
+	/*private bool boxIsMovingOnPlane()
+	{
+		//only returns true if box is moving on a plan i.e. only in x direction
+		if (Mathf.Abs (box.rigidbody.velocity.x) > 0 && Mathf.Abs (box.rigidbody.velocity.y) == 0 && Mathf.Abs (box.rigidbody.velocity.z) > 0)
+			return true;
+		return false;
+	}*/
+	
+	//automatically does collision for the dude (bc attached to this script)
+	//these funcitons are not called
+	public void OnCollisionEnter(Collision collision){
+		//collision and you are actually moving it
+		if (collision.gameObject.name == "CubeBlockingDoor" && Mathf.Abs (box.rigidbody.velocity.x) > 0) {
+			if (!boxSource.isPlaying)
+				boxSource.Play(); //continuous playing as long as loop is checked in box audio source componenet
+			
+		}
+	}
+	
+	public void OnCollisionExit(Collision collision)
+	{
+		//collision exit of cube block door
+		if (collision.gameObject.name == "CubeBlockingDoor") {
+			boxSource.Stop (); //stop collision noise
+		}
+		
 	}
 
 }

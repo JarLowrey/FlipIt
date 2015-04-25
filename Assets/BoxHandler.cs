9b00bbe5ad518c04676d7ae49f50f3781d66a869
tamd2 <tanabtawi147@gmail.com>
2015-04-25 15:41:31 -0400

    hi

---
+++ Assets/BoxHandler.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxHandler : MonoBehaviour {
+	AudioSource boxSource;
+	GameObject box;
+
+	// Use this for initialization
+	void Start () {
+		box = GameObject.Find ("CubeBlockingDoor");
+		boxSource = box.GetComponent<AudioSource> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//automatically does collision for the dude (bc attached to this script)
+	//these funcitons are not called
+	public void OnCollisionEnter(Collision collision){
+		//collision and you are actually moving it
+		//all box collision goes here
+		if (collision.gameObject.name == "Dude") {
+			if (Mathf.Abs (box.rigidbody.velocity.x) > 0) {
+				if (!boxSource.isPlaying)
+					boxSource.Play (); //continuous playing as long as loop is checked in box audio source componenet	\
+			}
+		}
+	}
+
+	public void OnCollisionExit(Collision collision)
+	{
+		//collision exit of cube block door
+			boxSource.Stop (); //stop collision noise
+	}
+}
---
+++ Assets/MainCharacterStats.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCharacterStats : MonoBehaviour {
+
+	Animator animateTheDude;
+	MainLevelHandler mainScript;
+	bool isDead;
+	GravityHandler gravScript;
+
+	// Use this for initialization
+	void Start () {
+		animateTheDude = GetComponent<Animator> (); 
+		mainScript = GetComponent<MainLevelHandler>();
+		gravScript = GetComponent<GravityHandler> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//collision with guy as pertains to health/stats etc.
+	public void OnCollisionEnter(Collision collision){
+		//collision and you are actually moving it
+		//all box collision goes here
+		if (collision.gameObject.tag == "notDude") {
+			handleCrushCollision (collision.gameObject, this.gameObject); //whatever this script is attached to and the box
+		}
+	}
+	
+	public void OnCollisionExit(Collision collision)
+	{
+		
+		
+	}
+
+	//handle collison between two objects
+	//this funciton checks to see if something is crushed
+	//obj1 should be checked to see if it is crushing obj2
+	//assume that crusher can crush beingCrushed
+	private void handleCrushCollision(GameObject crusher, GameObject beingCrushed)
+	{
+		RaycastHit hit;
+		float velocityRequiredToKill = 3f;
+		bool onPlane = gravScript.isOnPlane ();
+		
+		bool crush = Physics.Raycast (beingCrushed.transform.position, Vector3.up, out hit,3); //true if the beingCrushed looks up and is being hit, 3 is hardcoded for now
+		Debug.Log (onPlane);
+		Debug.Log (crusher.rigidbody.velocity.y);
+		//options, play with mass, make a ratio of mass/velocity? extension ideas
+		if (crush && Mathf.Abs (crusher.rigidbody.velocity.y) >= velocityRequiredToKill) {
+			//GameObject.Destroy (beingCrushed);
+			//newLoad (Application.loadedLevelName);
+			animateTheDude.SetTrigger ("die");
+			mainScript.ShowDeathGui();
+		}
+	}
+	
+
+}
